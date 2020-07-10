8c408411ca919e8f7a13a8a78873c0939591c9bc
Keflem trindade <keflemtn@gmail.com>
2020-07-10 18:44:51 -0400

    painel, tentativas

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -6,4 +6,6 @@ import {Component} from '@angular/core';
     templateUrl: './topo.component.html',
     styleUrls: ['topo.component.css']
 })
-export class TopoComponent {}
+export class TopoComponent {
+    public titulo: string = 'Aprendendo Inglês';
+}
