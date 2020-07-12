345416ff53447935c1064e09cc1265c728fb9fb6
Keflem trindade <keflemtn@gmail.com>
2020-07-11 21:05:01 -0400

    ajustando tentativas

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Coracao} from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
@@ -10,6 +11,12 @@ export class TentativasComponent implements OnInit {
 
   public coracaoCheio: string = "/assets/coracao_cheio.png";
   public coracaoVazio: string = '/assets/coracao_vazio.png';
+
+  public coracoes: Array<Coracao> = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ];
 
   constructor() { }
 
